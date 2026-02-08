2276f6d1ed6e9e607440286dbfe26cea433d30a8
Andy <andydchoo@gmail.com>
2026-02-08 15:02:21 -0500

    check_resources functionality, basic testing conditionals implemented at end with instance

---
+++ main.py
@@ -0,0 +1,78 @@
+### Data ###
+recipes = {
+    "small": {
+        "ingredients": {
+            "bread": 2,  ## slice
+            "ham": 4,  ## slice
+            "cheese": 4,  ## ounces
+        },
+        "cost": 1.75,
+    },
+    "medium": {
+        "ingredients": {
+            "bread": 4,  ## slice
+            "ham": 6,  ## slice
+            "cheese": 8,  ## ounces
+        },
+        "cost": 3.25,
+    },
+    "large": {
+        "ingredients": {
+            "bread": 6,  ## slice
+            "ham": 8,  ## slice
+            "cheese": 12,  ## ounces
+        },
+        "cost": 5.5,
+    }
+}
+
+resources = {
+    "bread": 12,  ## slice
+    "ham": 18,  ## slice
+    "cheese": 24,  ## ounces
+}
+### Complete functions ###
+
+class SandwichMachine:
+    # Initializes values inside of the sandwich machine class
+    def __init__(self, machine_resources):
+        """Receives resources as input.
+           Hint: bind input variable to self variable"""
+        self.machine_resources = machine_resources
+
+    def check_resources(self, ingredients):
+        """Returns True when order can be made, False if ingredients are insufficient."""
+        # Conditional to pull from recipes and check if the resources are sufficient enough
+        # for the specified recipe
+        if self.machine_resources["bread"] >= recipes[ingredients]["ingredients"]["bread"]:
+            return print("True")
+        else:
+            return print("False")
+
+    def process_coins(self):
+        """Returns the total calculated from coins inserted.
+           Hint: include input() function here, e.g. input("how many quarters?: ")"""
+
+    def transaction_result(self, coins, cost):
+        """Return True when the payment is accepted, or False if money is insufficient.
+           Hint: use the output of process_coins() function for cost input"""
+
+    def make_sandwich(self, sandwich_size, order_ingredients):
+        """Deduct the required ingredients from the resources.
+           Hint: no output"""
+
+### Make an instance of SandwichMachine class and write the rest of the codes ###
+p1 = SandwichMachine(resources)
+
+print("What would you like? (small/ medium/ large/ off/ report)")
+size = input()
+# Taking input for resource size and then inserting into search function
+if size in ("small", "medium", "large"):
+    p1.check_resources(size)
+elif size == "off":
+    quit()
+elif size == "report":
+    print(p1.machine_resources)
+    print(f"Size: {size}")
+else:
+    print("Invalid input")
